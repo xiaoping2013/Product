0756a5a54f82c2a7200dfc0cc544e57c730def57
肖平 <364878853@qq.com>
2013-03-12 13:44:50 +0800

    xiaoping

--- models/product.js
+++ models/product.js
@@ -7,3 +7,19 @@ var GridStore = mongo.GridStore;
 var Server = mongo.Server;
 var ReplSetServers = mongo.ReplSetServers;
 var Db = mongo.Db;
+var result = {};
+exports.queryMetadata = function(req,res){
+    try{
+        _queryMetadata(req,res);
+    }catch (ex){
+        result.code = '500';
+        result.message = '服务器发生错误';
+        result.isSuccessed = false;
+        result.result = {};
+        res.send(result);
+        res.end();
+    }
+};
+var _queryMetadata = function(req,res){
+
+};
